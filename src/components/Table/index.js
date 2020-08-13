0b03fd5a273986ac275a5e14037fb72a869a72a9
蔡瑞斌 <15934545664@163.com>
2020-08-13 21:03:18 +0800

    [b0]加了一行个taBLE

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,14 +1,180 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
+import { Table } from 'antd';
+import { DndProvider, DragSource, DropTarget } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import update from 'immutability-helper';
 
-class OTable extends Component {
+let dragingIndex = -1;
+
+class BodyRow extends React.Component {
     render() {
-        return (
-            <>
+        const { isOver, connectDragSource, connectDropTarget, moveRow, ...restProps } = this.props;
+        const style = { ...restProps.style, cursor: 'move' };
 
-            </>
-        )
+        let { className } = restProps;
+        if (isOver) {
+            if (restProps.index > dragingIndex) {
+                className += ' drop-over-downward';
+            }
+            if (restProps.index < dragingIndex) {
+                className += ' drop-over-upward';
+            }
+        }
+
+        return connectDragSource(
+            connectDropTarget(<tr {...restProps} className={className} style={style} />),
+        );
     }
 }
+
+const rowSource = {
+    beginDrag(props) {
+        dragingIndex = props.index;
+        return {
+            index: props.index,
+        };
+    },
+};
+
+const rowTarget = {
+    drop(props, monitor) {
+        const dragIndex = monitor.getItem().index;
+        const hoverIndex = props.index;
+        if (dragIndex === hoverIndex) {
+            return;
+        }
+        props.moveRow(dragIndex, hoverIndex);
+        monitor.getItem().index = hoverIndex;
+    },
+};
+
+const DragableBodyRow = DropTarget('row', rowTarget, (connect, monitor) => ({
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+}))(
+    DragSource('row', rowSource, connect => ({
+        connectDragSource: connect.dragSource(),
+    }))(BodyRow),
+);
+
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+        key: 'address',
+    },
+];
+
+class DragSortingTable extends React.Component {
+    state = {
+        data: [
+            {
+                key: '1',
+                name: 'John Brown',
+                age: 32,
+                address: 'New York No. 1 Lake Park',
+            },
+            {
+                key: '2',
+                name: 'Jim Green',
+                age: 42,
+                address: 'London No. 1 Lake Park',
+            },
+            {
+                key: '3',
+                name: 'Joe Black',
+                age: 32,
+                address: 'Sidney No. 1 Lake Park',
+            }, {
+                key: '4',
+                name: 'Jim Green',
+                age: 42,
+                address: 'London No. 1 Lake Park',
+            },
+            {
+                key: '5',
+                name: 'Joe Black',
+                age: 32,
+                address: 'Sidney No. 1 Lake Park',
+            }, {
+                key: '6',
+                name: 'John Brown',
+                age: 32,
+                address: 'New York No. 1 Lake Park',
+            },
+            {
+                key: '7',
+                name: 'Jim Green',
+                age: 42,
+                address: 'London No. 1 Lake Park',
+            },
+            {
+                key: '8',
+                name: 'Joe Black',
+                age: 32,
+                address: 'Sidney No. 1 Lake Park',
+            }, {
+                key: '9',
+                name: 'Jim Green',
+                age: 42,
+                address: 'London No. 1 Lake Park',
+            },
+            {
+                key: '10',
+                name: 'Joe Black',
+                age: 32,
+                address: 'Sidney No. 1 Lake Park',
+            },
+        ],
+    };
+
+    components = {
+        body: {
+            row: DragableBodyRow,
+        },
+    };
+
+    moveRow = (dragIndex, hoverIndex) => {
+        const { data } = this.state;
+        const dragRow = data[dragIndex];
+        console.log(dragIndex, hoverIndex)
+        this.setState(
+            update(this.state, {
+                data: {
+                    $splice: [[dragIndex, 1], [hoverIndex, 0, dragRow]],
+                },
+            }),
+        );
+    };
+
+    render() {
+        return (
+            <DndProvider backend={HTML5Backend}>
+                <Table
+                    columns={columns}
+                    dataSource={this.state.data}
+                    components={this.components}
+                    onRow={(record, index) => ({
+                        index,
+                        moveRow: this.moveRow,
+                    })}
+                />
+            </DndProvider>
+        );
+    }
+}
+
 const mapStateToProps = () => {
     return {
 
@@ -21,4 +187,4 @@ const mapDispatchToProps = () => {
 
     };
 }
-export default connect(mapStateToProps, mapDispatchToProps)(OTable)
+export default connect(mapStateToProps, mapDispatchToProps)(DragSortingTable)
