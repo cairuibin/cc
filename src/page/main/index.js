d5f7bb277d1ee86f884e839f6c70dadfd97f5da4
蔡瑞斌 <15934545664@163.com>
2020-08-20 09:53:36 +0800

    [t0]修改了一部的二二二分

--- src/page/main/index.js
+++ src/page/main/index.js
@@ -19,15 +19,28 @@ const mapDispatchToProps = (dispatch) => {
     };
 }
 
-
+const arr=[{
+    path:"/main/resource",
+    key:"0",
+    name:"资源"
+},{
+    path:"/main/sanshi",
+    key:"0",
+    name:"直播"
+}]
 export default connect(mapStateToProps, mapDispatchToProps)(class Main extends React.Component {
 
     render() {
         return (
             <div className='main_container'>
                 <div className='header_menu'>
-                    <div onClick={() => this.props.history.push('/main/resource')}>资源</div>
-                    <div onClick={() => this.props.history.push('/main/sanshi')}>三十</div>
+    
+                    <img style={{width:"149px",height:"50px",display:"block"}} src={require('../../assets/img/logo.png')} alt=""/>
+                    {arr.map((v,i)=>{
+                        return  <div onClick={() => this.props.history.push(v.path)}>{v.name}</div>
+                    })}
+                   
+                    {/* <div onClick={() => this.props.history.push('/main/sanshi')}>三十</div> */}
                 </div>
                 <div className='container'>
                     <RouterView routers={this.props.routers} />
